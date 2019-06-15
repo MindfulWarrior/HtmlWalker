ff0c34e19a67293220841626a2145c56ef606bce
MindfulWarrior <7spheresma@gmail.com>
2019-06-15 15:15:25 -0400

    In DocCleaner, remove attributes to be handled more smartly later

--- CSharp/HtmlDocToolTest/CleanerTest.cs
+++ CSharp/HtmlDocToolTest/CleanerTest.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Html5DomWalker;
 using HtmlDocCleaner;
 using HtmlTest.Base;
@@ -46,19 +47,41 @@ namespace HtmlDocToolTest
             DoTest(testInput, testOutput, expected, options);
         }
 
+        protected void DoWordTest(string test)
+        {
+            var options = new TestOptions(Platform) { AutoCreate = false };
+            options.DocumentOptions.Encoding = CodePagesEncodingProvider.Instance.GetEncoding(1252);
+
+            var testInput = GetTestInput("test." + test + ".html");
+            var expected = GetTestExpected("saved." + test + ".html", testInput, options);
+            var testOutput = GetTestOutput("output." + test + ".html");
+            DoTest(testInput, testOutput, expected, options);
+        }
+
         [TestMethod]
+        [TestCategory("Html5")]
         public void Comment_01() => DoTest("comment.01");
 
         [TestMethod]
+        [TestCategory("Html5")]
         public void Head_01() => DoTest("head.01");
 
         [TestMethod]
+        [TestCategory("Html5")]
         public void Div_01() => DoTest("div.01");
 
         [TestMethod]
+        [TestCategory("Html5")]
         public void Attribute_01() => DoTest("attribute.01");
 
         [TestMethod]
-        public void Word_01() => DoTest("word.01");
+        [TestCategory("Word")]
+        [Description("One line paragrach")]
+        public void Word_01() => DoWordTest("word.01");
+
+        [TestMethod]
+        [TestCategory("Word")]
+        [Description("Bullet list")]
+        public void Word_02() => DoWordTest("word.02");
     }
 }
