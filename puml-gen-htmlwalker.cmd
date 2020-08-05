104ae405f9d543a04314d205e77a922795d72534
MindfulWarrior <7spheresma@gmail.com>
2020-08-05 14:37:19 -0400

    UML Documentation using Puml

---
+++ puml-gen-htmlwalker.cmd
@@ -0,0 +1,2 @@
+puml-gen CSharp\HtmlWalker Documentation\HtmlWalker -dir -public -excludePaths Tag.Html,bin,obj -createAssociatons -allInOne
+puml-gen CSharp\HtmlWalker\Exception Documentation\HtmlWalker -dir -public -createAssociatons -allInOne
